13222873abf113d4d8327842e9da202923642667
santiago <ramossoloyo12345@gmail.com>
2025-11-28 14:12:00 -0500

    Refactorizacion de las dependencias. Se utiliza Annonated.

--- src/modules/catalogo/api/dependencies.py
+++ src/modules/catalogo/api/dependencies.py
@@ -3,15 +3,18 @@ Funciones de Inyección de Dependencias para el módulo de Catálogo.
 """
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi import Depends
+from typing import Annotated
 
 from src.core.database import get_db_session
 from src.modules.catalogo.infrastructure.repositories import SQLAlchemyProductRepository
 from src.modules.catalogo.application.features.create_product.use_case import CreateProductUseCase
 
+from src.modules.catalogo.application.features.list_products.use_case import ListProductsUseCase
+
 
 # Dependency: Product Repository
 async def get_product_repository(
-    session: AsyncSession = Depends(get_db_session)
+    session: Annotated[AsyncSession, Depends(get_db_session)] 
 ) -> SQLAlchemyProductRepository:
     """
     Inyecta el repositorio de productos.
@@ -27,7 +30,7 @@ async def get_product_repository(
 
 # Dependency: CreateProduct Use Case
 async def get_create_product_use_case(
-    repository: SQLAlchemyProductRepository = Depends(get_product_repository)
+    repository: Annotated[SQLAlchemyProductRepository, Depends(get_product_repository)]
 ) -> CreateProductUseCase:
     """
     Inyecta el caso de uso CreateProduct.
@@ -43,8 +46,8 @@ async def get_create_product_use_case(
 
 # Dependency: ListProducts Use Case
 async def get_list_products_use_case(
-    repository: SQLAlchemyProductRepository = Depends(get_product_repository)
+    repository: Annotated[SQLAlchemyProductRepository, Depends(get_product_repository)]
 ) -> "ListProductsUseCase":
     """Inyecta el caso de uso ListProducts."""
-    from src.modules.catalogo.application.features.list_products.use_case import ListProductsUseCase
+    
     return ListProductsUseCase(repository)
